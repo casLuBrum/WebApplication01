2889f55ffc2749719aed619709ec82ff35c3fddc
Lucas Brum <lucas_trabach@hotmail.com>
2017-06-20 09:42:07 -0300

    Iniciando Projeto

---
+++ WebApplication1/Models/Ficha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Ficha
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+        public int Sexo { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ WebApplication1/Models/FichaService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class FichaService
+    {
+        static List<Ficha> fichas = new List<Ficha>();
+        public FichaService()
+        {
+            for(int i = 0; i < 30; i++)
+            {
+                Ficha ficha = new Ficha();
+                ficha.Id = i;
+                ficha.Ativo = true;
+                ficha.DataNascimento = DateTime.Now;
+                ficha.Nome = "Nome " + i;
+                ficha.Sexo = i % 2;
+                fichas.Add(ficha);
+            }
+        }
+
+        public List<Ficha> ObterTodasFichas()
+        {
+            return fichas;
+        }
+
+        public Ficha ObterFicha(int id)
+        {
+            return fichas[id];
+        }
+
+        public void alterar(Ficha ficha)
+        {
+            fichas[ficha.Id] = ficha;
+        }
+    }
+}
